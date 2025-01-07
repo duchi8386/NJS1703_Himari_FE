5f3bad123157d1d9f3198ed6ae9b811503e60bf3
Trong Minh Do <156078684+trongminhSE172130@users.noreply.github.com>
2025-01-07 13:55:39 +0700

    update router

---
+++ SWD392-FE/src/routers/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "../page/HomePage";
+import RootLayout from "../layout/RootLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
