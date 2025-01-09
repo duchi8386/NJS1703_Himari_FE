1b1c7b78a76e81278e7edd8597fa61ac70e4a4ff
Ducctri <duchitran2410@gmail.com>
2025-01-09 22:58:42 +0700

    xoa hero section

--- SWD392-FE/src/layout/RootLayout.jsx
+++ SWD392-FE/src/layout/RootLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
-import HeroSection from "../components/hero/heroSection";
 import Footer from "../components/footer/Footer";
 
 function RootLayout() {
   return (
     <>
       <Header />
-      <HeroSection />
       <main>
         <div className="flex justify-center overflow-hidden py-16">
           <div className="md:w-[85%] w-[95%]">
