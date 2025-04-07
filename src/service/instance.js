952525a7a53f807ce6397210d95367ade8af9028
lion3993vn <lion3993vn@gmail.com>
2025-04-07 22:03:38 +0700

    fix: base url

--- src/service/instance.js
+++ src/service/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://wizlab.io.vn:9999/api/v1/";
+const API_BASE_URL = "https://himari-api.wizlab.io.vn/api/v1/";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
